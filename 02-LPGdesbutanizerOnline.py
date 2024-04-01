0f5b6676981ff9aefb120e7075fc3a6e5d5a0e34
Davi Belo <davibelo@gmail.com>
2024-03-31 22:48:30 -0300

    read to test

--- 02-LPGdesbutanizerOnline.py
+++ 02-LPGdesbutanizerOnline.py
@@ -1,50 +1,66 @@
+import time
 import subprocess
+from src.mpc import mpc_controller_scipy_minimize
 
+# MPC parameters
+EXECUTION_TIME = 30 # Execution time in seconds
+T = 1  # Sampling time (min)
+ny = 2
+nu = 2
+n = 120  # Stabilizing horizon
+p = 120  # Output prediction horizon
+m = 5  # Control horizon
+umax = np.array([200., 20000.])
+umin = np.array([120., 1000.])
+ymax = np.array([0.02, 0.01])
+ymin = np.array([0.01, 0.005])
+dumax = np.array([1, 1000.])
+q = np.array([1000., 1000.])  # Output weights
+r = np.array([100., 0.00001])  # Input weights
+
+# OPC parameters
 OPC_SERVER = 'Matrikon.OPC.Simulation.1'
 HOST = 'localhost'
 tags_to_read = ['APD.C2LPG', 'APD.C5LPG', 'APD.TBOTTOMSP', 'APD.QREFLUXSP']
 
-# Define the command and arguments
-command = 'C:\\Program Files\\OpenOPC\\bin\\opc.exe'
-server_args = [f'--server={OPC_SERVER}', f'--host={HOST}']
-tags_to_read_args = ['--read'] + tags_to_read + ['--output=values']
+while True:
+    start_time = time.time()  # Get the current time
+    # Reading plant with OpenOPC CLI
+    command = 'C:\\Program Files\\OpenOPC\\bin\\opc.exe'
+    server_args = [f'--server={OPC_SERVER}', f'--host={HOST}']
+    tags_to_read_args = ['--read'] + tags_to_read + ['--output=values']
+    cmd = [command] + server_args + tags_to_read_args
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    output = result.stdout
+    lines = output.split('\n')
+    C2LPG, C5LPG, TBOTTOMSP, QREFLUXSP = float(lines[0]), float(lines[1]), float(lines[2]), float(
+        lines[3])
+    print('reading tags...')
+    print(f'C2LPG: {C2LPG}')
+    print(f'C5LPG: {C5LPG}')
+    print(f'TBOTTOMSP: {TBOTTOMSP}')
+    print(f'QREFLUXSP: {QREFLUXSP}\n')
 
-# Combine the command and arguments into a single list
-cmd = [command] + server_args + tags_to_read_args
+    # Call MPC
+    y0temp = np.array([C2LPG, C5LPG], dtype=float)
+    u0temp = np.array([TBOTTOMSP, QREFLUXSP], dtype=float)
+    u_opt = mpc_controller_scipy_minimize(ny, nu, T, n, p, m, umax, umin, ymax, ymin, dumax, q, r,
+                                          u0temp, y0temp, Gmstep)
+    TBOTTOMSP, QREFLUXSP = u_opt.reshape(-1, 1)
+    print('writing tags...')
+    print('TBOTTOMSP:', TBOTTOMSP)
+    print('QREFLUXSP:', QREFLUXSP)
 
-# Execute the command and capture the output
-result = subprocess.run(cmd, capture_output=True, text=True)
+    # Writing to plant with OpenOPC CLI
+    write_args = ['--write', 'APD.TBOTTOMSP', str(TBOTTOMSP), 'APD.QREFLUXSP', str(QREFLUXSP)]
+    cmd = [command] + write_args
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    print(result.stdout)
+    end_time = time.time()
 
-# The stdout attribute contains the output
-output = result.stdout
-lines = output.split('\n')
-
-# Assign each line to a separate variable
-C2LPG, C5LPG, TBOTTOMSP, QREFLUXSP = float(lines[0]), float(lines[1]), float(lines[2]), float(lines[3])
-
-# Print the output
-print('reading tags...')
-print(f'C2LPG: {C2LPG}')
-print(f'C5LPG: {C5LPG}')
-print(f'TBOTTOMSP: {TBOTTOMSP}')
-print(f'QREFLUXSP: {QREFLUXSP}\n')
-
-# New values to write
-TBOTTOMSP = 120.2
-QREFLUXSP = 670.2
-
-print('writing tags...')
-print('TBOTTOMSP:', TBOTTOMSP)
-print('QREFLUXSP:', QREFLUXSP)
-
-# Arguments to write new values
-write_args = ['--write', 'APD.TBOTTOMSP', str(TBOTTOMSP), 'APD.QREFLUXSP', str(QREFLUXSP)]
-
-# Combine the command and arguments into a single list
-cmd = [command] + write_args
-
-# Execute the command
-result = subprocess.run(cmd, capture_output=True, text=True)
-
-# Print the output
-print(result.stdout)
+    # Calculate sleep time
+    elapsed_time = end_time - start_time
+    time_to_wait = EXECUTION_TIME - elapsed_time
+    print(f'Elapsed time: {elapsed_time} seconds')
+    print(f'Waiting {time_to_wait} seconds...\n')
+    time.sleep(time_to_wait)
