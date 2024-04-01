a3409a9573a64c59c6fc9b8f29e51c918afc579c
Davi Belo <davibelo@gmail.com>
2024-04-01 11:04:46 -0300

    pv tracking

--- 02-LPGdesbutanizerOnline.py
+++ 02-LPGdesbutanizerOnline.py
@@ -17,27 +17,58 @@ p = 120  # Output prediction horizon
 m = 5  # Control horizon
 umax = np.array([200., 20000.])
 umin = np.array([120., 1000.])
-# ymax = np.array([0.015, 0.016])
-# ymin = np.array([0.013, 0.015])
-ymax = np.array([0.015, 0.010])
-ymin = np.array([0.013, 0.005])
-dumax = np.array([2, 2000.])
+# ymax = np.array([0.015, 0.016]) # run initials
+# ymin = np.array([0.013, 0.015]) # run initials
+ymax = np.array([0.015, 0.010]) # run 1
+ymin = np.array([0.013, 0.009]) # run 1
+dumax = np.array([0.5, 500.])
 q = np.array([1000., 1000.])  # Output weights
 r = np.array([100., 0.00001])  # Input weights
 
 # OPC parameters
 OPC_SERVER = 'Matrikon.OPC.Simulation.1'
 HOST = 'localhost'
-tags_to_read = ['APD.C2LPG', 'APD.C5LPG', 'APD.TBOTTOMSP', 'APD.QREFLUXSP']
 
 #load the step responses
 Gmstep = joblib.load('outputs/Gstep.joblib')
+
+# Initialize SPs with PV values
+tags_to_read = ['APD.TBOTTOMPV', 'APD.QREFLUXPV']
+
+# Reading plant with OpenOPC CLI
+command = 'C:\\Program Files\\OpenOPC\\bin\\opc.exe'
+server_args = [f'--server={OPC_SERVER}', f'--host={HOST}']
+tags_to_read_args = ['--read'] + tags_to_read + ['--output=values']
+cmd = [command] + server_args + tags_to_read_args
+result = subprocess.run(cmd, capture_output=True, text=True)
+output = result.stdout
+lines = output.split('\n')
+TBOTTOMPV, QREFLUXPV = float(lines[0]), float(lines[1])
+print('Initializing SPs with PV values...')
+print(f'TBOTTOMPV: {TBOTTOMPV}')
+print(f'QREFLUXPV: {QREFLUXPV}\n')
+
+TBOTTOMSP = TBOTTOMPV
+QREFLUXSP = QREFLUXPV
+
+# Writing to plant with OpenOPC CLI
+write_args = ['--write', 'APD.TBOTTOMSP', str(TBOTTOMSP), 'APD.QREFLUXSP', str(QREFLUXSP)]
+cmd = [command] + write_args
+result = subprocess.run(cmd, capture_output=True, text=True)
+print('writing tags initial SPs...')
+print('TBOTTOMSP:', TBOTTOMSP)
+print('QREFLUXSP:', QREFLUXSP)
+
+# Wait for user input to start
+print('SPs initialized!')
+input('Press Enter to continue...')
 
 while True:
     start_time = time.time()  # Get the current time
     # Reading plant with OpenOPC CLI
     command = 'C:\\Program Files\\OpenOPC\\bin\\opc.exe'
     server_args = [f'--server={OPC_SERVER}', f'--host={HOST}']
+    tags_to_read = ['APD.C2LPG', 'APD.C5LPG', 'APD.TBOTTOMSP', 'APD.QREFLUXSP']
     tags_to_read_args = ['--read'] + tags_to_read + ['--output=values']
     cmd = [command] + server_args + tags_to_read_args
     result = subprocess.run(cmd, capture_output=True, text=True)
